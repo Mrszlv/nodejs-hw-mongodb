057db20dcb75cc710695d8c4386ad4b920b51ec1
Miroszlav <miroszlav.popovics@gmail.com>
2024-11-30 14:08:41 +0200

    commit-filter

--- src/servises/contacts.js
+++ src/servises/contacts.js
@@ -7,10 +7,18 @@ export const getAllContacts = async ({
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
   sortBy = 'name',
+  filter = {},
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
   const contactsQuery = ContactsColection.find();
+
+  if (filter.contactType) {
+    contactsQuery.where('contactType').equals(filter.contactType);
+  }
+  if (filter.isFavourite) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
 
   const [contactsCount, contacts] = await Promise.all([
     ContactsColection.find().merge(contactsQuery).countDocuments(),
